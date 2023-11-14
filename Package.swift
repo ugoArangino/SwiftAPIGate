fac51e809a3986082a7366a103885262f1075760
Ugo Arangino <ugo@arangino.de>
2023-11-14 16:06:14 +0100

    Downgrade to iOS 15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftAPIGate",
     platforms: [
-        .iOS(.v17),
+        .iOS(.v15),
         .macOS(.v10_15),
     ],
     products: [
